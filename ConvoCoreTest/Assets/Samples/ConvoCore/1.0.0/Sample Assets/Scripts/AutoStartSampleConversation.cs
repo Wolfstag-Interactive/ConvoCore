0ad65f7b06e4063741030186b013689b1a6d6fc3
Ornlu2 <alterdylan@gmail.com>
2025-10-01 12:32:41 -0600

    added action group action

--- ConvoCoreTest/Assets/Samples/ConvoCore/1.0.0/Sample Assets/Scripts/AutoStartSampleConversation.cs
+++ ConvoCoreTest/Assets/Samples/ConvoCore/1.0.0/Sample Assets/Scripts/AutoStartSampleConversation.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
-using WolfstagInteractive.ConvoCore;
-[RequireComponent(typeof(ConvoCore))]
+[RequireComponent(typeof(WolfstagInteractive.ConvoCore.ConvoCore))]
 public class AutoStartSampleConversation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<ConvoCore>().StartConversation();
+        GetComponent<WolfstagInteractive.ConvoCore.ConvoCore>().StartConversation();
     }
 }
---
+++ WolfstagInteractive/ConvoCore/Scripts/SampleActions/ConvoCoreAction_ActionGroup.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WolfstagInteractive.ConvoCore;
+
+namespace ConvoCore
+{
+    [CreateAssetMenu(menuName = "ConvoCore/Actions/Action Group")][ System.Serializable]
+    
+    public class ConvoCoreAction_ActionGroup : BaseAction
+    {
+        /// <summary>
+        /// Add commonly executed actions together for easy reuse, executes each action in the list one after the other.
+        /// </summary>
+        public List<BaseAction> ActionGroup = new List<BaseAction>();
+        
+        public override IEnumerator DoAction()
+        {
+            foreach (var t in ActionGroup)
+            {
+                yield return t.DoAction();
+            }
+        }
+    }
+}
