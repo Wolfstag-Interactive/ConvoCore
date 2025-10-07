a81d64eed1470663b2817480caaf0daf2add6627
Ornlu2 <Ornlu2@users.noreply.github.com>
2025-10-07 17:40:23 +0000

    deploy: 36dba922173fb7c1d70027ad286b94fd459c3b95

--- classWolfstagInteractive_1_1ConvoCore_1_1CharacterRepresentationBase.js
+++ classWolfstagInteractive_1_1ConvoCore_1_1CharacterRepresentationBase.js
@@ -1,6 +1,7 @@
 var classWolfstagInteractive_1_1ConvoCore_1_1CharacterRepresentationBase =
 [
     [ "DrawInlineEditorPreview", "classWolfstagInteractive_1_1ConvoCore_1_1CharacterRepresentationBase.html#aa6f7365c0cf21b80d4c02416aa436604", null ],
+    [ "GetEmotionMappingByGuid", "classWolfstagInteractive_1_1ConvoCore_1_1CharacterRepresentationBase.html#abef717a1035749413824b89b5480f2e7", null ],
     [ "GetPreviewHeight", "classWolfstagInteractive_1_1ConvoCore_1_1CharacterRepresentationBase.html#aa83c3e052546a175de00f0d60deda5f0", null ],
     [ "ProcessEmotion", "classWolfstagInteractive_1_1ConvoCore_1_1CharacterRepresentationBase.html#a80a7e4c552b3fadde711d3f3d402c16d", null ]
 ];
