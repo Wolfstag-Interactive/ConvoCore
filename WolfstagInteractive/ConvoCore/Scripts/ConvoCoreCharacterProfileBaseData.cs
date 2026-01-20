516d4afc103e3747962cb9d9e524eeb172c3a584
Ornlu2 <alterdylan@gmail.com>
2026-01-20 01:37:30 -0700

    started to build the 3D sample scene

--- WolfstagInteractive/ConvoCore/Scripts/ConvoCoreCharacterProfileBaseData.cs
+++ WolfstagInteractive/ConvoCore/Scripts/ConvoCoreCharacterProfileBaseData.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace WolfstagInteractive.ConvoCore
 {
     [UnityEngine.HelpURL("https://docs.wolfstaginteractive.com/convocore/api/classWolfstagInteractive_1_1ConvoCore_1_1ConvoCoreCharacterProfileBaseData.html")]
-[CreateAssetMenu(fileName = "NewCharacterProfile", menuName = "ConvoCore/CharacterProfile")]
+[CreateAssetMenu(fileName = "NewCharacterProfile", menuName = "ConvoCore/Character Profile")]
     public class ConvoCoreCharacterProfileBaseData : ScriptableObject
     {
         // Basic character information
